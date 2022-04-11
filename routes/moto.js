2ecdabf0f7b7a75a19e2c2a2e45b3a9765c6f296
BUDAHOOD <90276048+BUDAHOOD@users.noreply.github.com>
2022-04-11 18:41:24 -0400

    Update moto.js
    Se actualiza listado de rutas para Motos

--- routes/moto.js
+++ routes/moto.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {listar, crear} = require("../controllers/moto");
+const {crearMoto, detalleMoto, borradoSoftMoto, eliminaMoto, actualizaMoto, cuentaMotos, listaPagMoto} = require("../controllers/moto");
 
-router.post("/agregarMoto", crear);
-router.get("/listadomotos", listar);
+router.post("/agregarMoto", crearMoto);
+router.get("/detalleMoto/:slug", detalleMoto);
+router.patch("/moto/:slug", borradoSoftMoto);
+router.delete("/moto/:slug", eliminaMoto);
+router.put("/moto/:slug", actualizaMoto);
+router.get("/listaPag", listaPagMoto);
+router.get("/cuentaMotos", cuentaMotos);
 
 module.exports = router;
